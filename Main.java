151702064d4175d7e0ed94136394f0f96de7edcf
DuarteHermano <duartehermano10@gmail.com>
2026-03-11 17:05:20 +0000

    Add files via upload
    Main for Crystal Castle

--- Main.java
+++ Main.java
@@ -1,80 +1,53 @@
-package org.example;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.nio.Buffer;
 import java.util.*;
 
+
 public class Main {
+/* 
+ * ---Confirm if it´s necessary to check this constraints or if it is just moonshak tests specs---
+ * T->Number of Test Cases, 1<=T<=20
+ * R->Number of Rows, 1<=R<=400
+ * C->Number of Columns, 1<=C<=400
+ * M->Number of Consecutive Jumps, 1<=M<=5
+ * N->Max Jumps, 1<=N<=10
+ * 
+ * "." -> Tile with no constraints
+ * "X" -> Tile with no diagonal jumps (LD RD)
+ * "J" ->Tile with no jumps
+ * "X" -> Tile that cannot be stepped on 
+ * 
+ * ---Hints---
+ * Not supposed to store all the grids, just solve directly 1 by 1 and print out the result.
+ * Time complexity may be just about one case and not the multiple grids, but we have to specify on report that it is only for 1 case
+ * 
+ * 
+ * 
+ **/
+  public static void main(String[] args) throws Exception{
+    BufferedReader in = new BufferedReader(new java.io.InputStreamReader(System.in));
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+    int T =  Integer.parseInt(in.readLine().trim());
+    for(int t = 0; t < T;t++){
+      String line = in.readLine().trim();
+      String[] parts = line.split(" ");
 
-        int TestCases = Integer.parseInt(in.readLine().trim());
+      int R = Integer.parseInt(parts[0]);
+      int C = Integer.parseInt(parts[1]);
+      int M = Integer.parseInt(parts[2]);
+      int N = Integer.parseInt(parts[3]);
 
-
-        for (int i = 0; i < TestCases; i++) {
-            int numOfOffers = Integer.parseInt(in.readLine().trim());
-
-            Record[] characteristics = new Record[numOfOffers];
-
-            for(int j=0;j<numOfOffers;j++){
-                String offer = (in.readLine().trim());
-                String[] parts = offer.split(" ");
-                int startTime = Integer.parseInt(parts[0]);
-                int duration = Integer.parseInt(parts[1]);
-                int price = Integer.parseInt(parts[2]);
-
-                Record rec = new Record(startTime, duration, price);
-
-                characteristics[j] = rec;
-            }
-            Arrays.sort(characteristics);
-            System.out.println(dynamicProg(characteristics, numOfOffers));
+      //Process the grid
+      char[][] grid = new char[R][C];
+      for(int i = 0; i < R;i++){
+        String gridLine = in.readLine().trim();
+        for(int j = 0; j < C;j++){
+          grid[i][j] = gridLine.charAt(j);
         }
-
+      }
+      //Create a solver instance and get the result
+      Solver solver = new Solver(R, C, M , N, grid);
+      System.out.println(solver.getResult());
     }
-
-
-    private static int dynamicProg(Record[] characteristics, int numOfOffers) {
-        int[] max = new int[numOfOffers];
-        max[0] = characteristics[0].price;
-        for(int j=1;j<numOfOffers;j++){
-            Record rec = characteristics[j];
-            max[j] = Math.max(max[j-1], rec.price + search(characteristics, max, j));
-        }
-        Arrays.sort(max);
-        return max[numOfOffers-1];
-    }
-
-    private static int search(Record[] characteristics, int[] max,  int j) {
-        Record rec = characteristics[j];
-        for (int i=j-1;i>=0;i--){
-            Record rec2 = characteristics[i];
-            if(rec2.endTime<=rec.startTime){
-                return max[i];
-            }
-        }
-        return 0;
-    }
-
-    public static class Record implements Comparable {
-        int startTime;
-        int duration;
-        int price;
-        int endTime;
-        public Record(int startTime, int duration, int price) {
-            this.startTime = startTime;
-            this.duration = duration;
-            this.price = price;
-            this.endTime = startTime + duration;
-        }
-
-        @Override
-        public int compareTo(Object o) {
-            if(o instanceof Record){
-                return this.endTime - ((Record)o).endTime;
-            }
-            else return 0;
-        }
-    }
+  }
 }
-
