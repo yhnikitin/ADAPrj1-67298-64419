5bf6f21659924e9f8a6d6203e8e44d7c8c2b1d4c
Duarte Hermano <duartehermano10@exemplo.com>
2026-03-16 09:43:06 +0000

    Updated

--- SolverClass.java
+++ SolverClass.java
@@ -4,7 +4,7 @@ public class SolverClass implements Solver {
   private int M;
   private int N;
   private char[][] grid;
-  private static final int MOD = 1_000_000_007;
+  private static final int DIVISOR = 1000000007;
 
   public SolverClass(int R, int C, int M, int N, char[][] grid) {
     this.R = R;
@@ -15,7 +15,7 @@ public class SolverClass implements Solver {
   }
 
   @Override
-  //Method to solve the problem, currently returns 0 as a placeholder
+  //solve, process the grid and return the number of ways to reach the bottom-right corner
   public int solve() {
       int[][][][] dp = new int[R][C][M + 1][N + 1];
       dp[0][0][0][0] = 1;
@@ -29,28 +29,28 @@ public class SolverClass implements Solver {
                   for (int k = 0; k <= N; k++) {
 
                       if (c - 1 >= 0) {
-                          dp[r][c][0][k] = (dp[r][c][0][k] + dp[r][c - 1][j][k]) % MOD;
+                          dp[r][c][0][k] = (dp[r][c][0][k] + dp[r][c - 1][j][k]) % DIVISOR;
                       }
 
                       if (r - 1 >= 0) {
-                          dp[r][c][0][k] = (dp[r][c][0][k] + dp[r - 1][c][j][k]) % MOD;
+                          dp[r][c][0][k] = (dp[r][c][0][k] + dp[r - 1][c][j][k]) % DIVISOR;
                       }
 
                       if (r - 2 >= 0 && j > 0 && k > 0) {
                           if (grid[r - 2][c] != 'J' && grid[r - 2][c] != '#') {
-                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 2][c][j - 1][k - 1]) % MOD;
+                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 2][c][j - 1][k - 1]) % DIVISOR;
                           }
                       }
 
                       if (r > 0 && c + 1 < C && j > 0 && k > 0) {
                           if (grid[r - 1][c + 1] != 'J' && grid[r - 1][c + 1] != 'X' && grid[r - 1][c + 1] != '#') {
-                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 1][c + 1][j - 1][k - 1]) % MOD;
+                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 1][c + 1][j - 1][k - 1]) % DIVISOR;
                           }
                       }
 
                       if (r > 0 && c > 0 && j > 0 && k > 0) {
                           if (grid[r - 1][c - 1] != 'J' && grid[r - 1][c - 1] != 'X' && grid[r - 1][c - 1] != '#') {
-                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 1][c - 1][j - 1][k - 1]) % MOD;
+                              dp[r][c][j][k] = (dp[r][c][j][k] + dp[r - 1][c - 1][j - 1][k - 1]) % DIVISOR;
                           }
                       }
                   }
@@ -61,7 +61,7 @@ public class SolverClass implements Solver {
       int ans = 0;
       for (int j = 0; j <= M; j++) {
           for (int k = 0; k <= N; k++) {
-              ans = (ans + dp[R - 1][C - 1][j][k]) % MOD;
+              ans = (ans + dp[R - 1][C - 1][j][k]) % DIVISOR;
           }
       }
 
